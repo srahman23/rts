7e083c85be04325dc0171cf48d441d41d025a64b
Shahriar Rahman <shahriarrahman1999@gmail.com>
2020-03-26 04:16:45 -0700

    Update NodeEdit

--- Assets/Scripts/NodeEdit.cs
+++ Assets/Scripts/NodeEdit.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+/*
+ * Allows map editing with mouse clicks.
+ * Only paints the clicked tile red for now. Can be expanded for building placement and other uses.
+ */
 public class NodeEdit : MonoBehaviour
 {
 
@@ -15,16 +19,26 @@ public class NodeEdit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //If right-clicked
         if (Input.GetButtonDown("Fire1"))
         {
-            Vector3 clickPos = new Vector3(0, 0, 0);
+
+            Vector3 clickPos;
+
+            /*
+             * Creates a plane that is aligned with the terrain. 
+             * We then raycast and check the distance from mouse pointer to the plane.
+             * The distance also happens to be the distance from mouse pointer to the terrain as long as the terrain is flat.
+             */
+
             Plane plane = new Plane(Vector3.up, 0f);
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            float distanceToPlane;
-
+            //If the ray collides with the plane, it will output the ray to distanceToPlane
+            float distanceToPlane; 
             if(plane.Raycast(ray, out distanceToPlane))
             {
+
                 clickPos = ray.GetPoint(distanceToPlane);
                 print(clickPos);
                 updateMap(clickPos);
@@ -34,11 +48,13 @@ public class NodeEdit : MonoBehaviour
     }
     void updateMap(Vector3 position)
     {
-
+        //Rounds the world coordinates to the nearest tile value before sending to MapGen.
         int xPos = (int)Math.Round(position.x / 10.0);
         int zPos = (int)Math.Round(position.z / 10.0);
        
-        print(xPos +" "+zPos);
+        //Debug line if you want to check.
+        //print(xPos +" "+zPos);
+
         mapGen.updatePosition(xPos, zPos);
     }
 }
